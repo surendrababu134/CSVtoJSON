c5b18efb186969a436885af3e2b699f3d634db40
Surendra Babu <surendra134@gmail.com>
2017-07-04 15:22:45 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+//require the csvtojson converter class
+var Converter = require("csvtojson").Converter;
+// create a new converter object
+var converter = new Converter({});
+
+// call the fromFile function which takes in the path to your
+// csv file as well as a callback function
+converter.fromFile("CSVtoJSON.csv",function(err,result){
+    // if an error has occured then handle it
+    if(err){
+        console.log("An Error Has Occured");
+        console.log(err);
+    }
+    // create a variable called json and store
+    // the result of the conversion
+    var json = result;
+    console.log(json);
+});
